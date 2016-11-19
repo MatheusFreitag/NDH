81df9683f11f5c58f4a6f417c4f24be6789ba929
Matheus Eduardo Freitag <freitag.matheus@gmail.com>
2016-11-19 19:57:08 +0000

    Admin pode deletar usuarios

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!, :except => [:show]
+
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def delete
+   User.find(params[:id]).destroy
+   redirect_to :action => 'index'
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  match '/users/:id', to: 'users#show', via: 'get'
+  match '/users',   to: 'users#index',   via: 'get'
+  match 'users/:id' => 'users#delete', :via => :delete, :as => :admin_destroy_user
+
   resources :pessoas
   resources :processos
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only =>[:show]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
